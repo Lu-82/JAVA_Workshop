0db395546f201a77b018b017c5cad4165740fc59
Lu-82 <raghunathmurmu353@gmail.com>
2026-06-23 13:42:05 +0530

    Add Solution

---
+++ GFG/DSA/Array/Array_Reversal.java
@@ -0,0 +1,25 @@
+package GFG.DSA.Array;
+
+public class Array_Reversal {
+    public static void main(String[] args) {
+        int []arr = {1,4,3,2,6,5};
+        reverseArray(arr);
+        for(int num : arr) {
+            System.out.print(num+" ");
+        }
+    }
+
+    public static void reverseArray(int[] arr) {
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left <= right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+}
